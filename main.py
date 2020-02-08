d0d72ed7b3d294ec67cb473e8f6a4ff41d6917f1
Riza Sal <rza108@gmail.com>
2020-02-08 18:38:59 +0530

    Added kill switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import curses
+import sys
 
 class Dino:
     def __init__(self, stdscr):
@@ -13,29 +14,17 @@ class Dino:
         if self.y > 1:
             self.y -= 1
 
-    def destroy(self):
-        # clear the screen
-        stdscr.clear()
-
-        # reverse terminal settings
-        curses.nocbreak()
-        stdscr.keypad(False)
-        curses.echo()
-
-        # close the application
-        curses.endwin()
-        
-        
 class Game:
     def __init__(self):
 
         self.stdscr = curses.initscr()
-
         # tweak terminal settings
         curses.noecho()
         curses.cbreak()
         self.stdscr.keypad(True)
         curses.curs_set(0)
+        self.stdscr.nodelay(1)
+
 
         # write something on the screen
         self.stdscr.addstr(5, 10, "Hello, world!")
@@ -43,23 +32,37 @@ class Game:
 
         # update the screen
         self.stdscr.refresh()
-    
+
     def update(self):
         self.stdscr.clear()
+        k = self.stdscr.getch()
+        if k == 27:
+            self.destroy()
+            sys.exit()
+
         self.dino.update()
         self.stdscr.refresh()
+
+    def destroy(self):
+        # clear the screen
+        self.stdscr.clear()
+
+        # reverse terminal settings
+        curses.nocbreak()
+        self.stdscr.keypad(False)
+        curses.echo()
+
+        # close the application
+        curses.endwin()
 
 if __name__ == "__main__":
     frame_rate = 30
     prev = 0
     game = Game()
+
     while True:
         time_elapsed = time.time() - prev
-
         if time_elapsed > 1./frame_rate:
             prev = time.time()
             game.update()
-
-# wait for 3 seconds
-time.sleep(3)
 
