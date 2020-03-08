9ef206e5c7a2f775500977896133556608ffe74f
Riza Sal <rza108@gmail.com>
2020-03-08 17:32:26 +0530

    Working copy

--- main.py
+++ main.py
@@ -2,46 +2,80 @@ import time
 import curses
 import sys
 import math
-
+import random
 import locale
 
 locale.setlocale(locale.LC_ALL, '')
 code = locale.getpreferredencoding()
 
-class Cactus:
-    def __init__(self, x, y, stdscr):
+class Obstacle:
+    def __init__(self, game_object, x, y, h):
         self.x = x
-        self.y = y
+        self.y = y - h
+        self.h = h
 
-        self.jumping = False
-        self.stdscr = stdscr    
+        self.obstacle_map =[
+            [1]
+        ] * h
 
+        self.stdscr = game_object.stdscr
+        self.game_object = game_object
+
+    def draw(self):
+        self.update()
+        for row_index, i in enumerate(self.obstacle_map):
+            for col_index, j in enumerate(i):
+                if j == 1:
+                    self.stdscr.addstr(
+                        self.y + row_index,
+                        int(self.x) +col_index,
+                        bytes(u'\u2588'.encode(code)))
+    
     def update(self):
-        if self.x > 0:
-            self.x -=1
-            self.stdscr.addstr(self.y, self.x, 'dino!')
+        self.x -= 1.5
+
+        if self.x < 3:
+            self.game_object.array_obstacles.remove(self)
+
+        if self.x < self.game_object.max_w / 4:
+            dino = self.game_object.dino
+
+            if dino.x-1 <= self.x <= (dino.x + dino.dino_box_dimension[0]):
+                if dino.y <= self.y <=dino.y + dino.dino_box_dimension[1] or dino.y <= self.y+ self.h <=dino.y + dino.dino_box_dimension[1]:
+                    self.game_object.destroy()
+                    sys.exit()
+
 
 class Dino:
     def __init__(self, stdscr):
         self.x = 10
-        self.y = 15
+        self.y = 30
 
         self.dy = self.dy_yield()
         self.jumping = False
         self.stdscr = stdscr
 
-        self.dino_map = [
-            [0,0,0,0,0,1],
-            [1,1,1,1,1,0],
-            [1,1,1,1,1,0],
-            [1,0,0,0,1,0],
-            [1,0,0,0,1,0],
-        ]
+        self.dino_map_array_pointer = 0
+        self.dino_map = [[
+            [0,0,0,0,0,0,0,1,1,0],
+            [0,1,1,1,1,1,1,1,1,1],
+            [0,1,1,1,1,1,1,0,0,0],
+            [0,0,1,0,0,0,0,1,0,0],
+        ],
+        [
+            [0,0,0,0,0,0,0,1,1,0],
+            [0,1,1,1,1,1,1,1,1,1],
+            [0,1,1,1,1,1,1,0,0,0],
+            [0,1,0,0,0,0,1,0,0,0],
+        ]]
+        self.use_dino_map = 0
+
+        self.dino_box_dimension = (len(self.dino_map), len(self.dino_map[0]))
     
     def dy_yield(self):
-        for i in range(5):
+        for i in range(7):
            yield i * -1
-        for i in range(5):
+        for i in range(7):
            yield i
     
     def jump(self):
@@ -61,13 +95,17 @@ class Dino:
         self.draw()
     
     def draw(self):
-        for row_index, i in enumerate(self.dino_map):
+        if self.dino_map_array_pointer > 5:
+            self.use_dino_map = (self.use_dino_map +1 ) % 2
+            self.dino_map_array_pointer = 0
+        for row_index, i in enumerate(self.dino_map[self.use_dino_map]):
             for col_index, j in enumerate(i):
                 if j == 1:
                     self.stdscr.addstr(
                         self.y + row_index,
                         self.x +col_index,
-                        bytes(u'\u2588'''.encode(code)))
+                        bytes(u'''\u2588'''.encode(code)))
+        self.dino_map_array_pointer += 1
 
 class Game:
     def __init__(self):
@@ -79,8 +117,14 @@ class Game:
         self.stdscr.keypad(True)
         curses.curs_set(0)
         self.stdscr.nodelay(1)
+        self.max_h, self.max_w = self.stdscr.getmaxyx()
 
         self.dino = Dino(self.stdscr)
+
+        self.array_obstacles = [
+            Obstacle(self, 90, 35, 2)
+        ]
+        self.next_obstacle_generate_time = 0
 
         # update the screen
         self.stdscr.refresh()
@@ -95,6 +139,14 @@ class Game:
         elif k == 97:
             self.dino.jump()
 
+        for obstacle in self.array_obstacles:
+            obstacle.draw()
+
+        if self.next_obstacle_generate_time - time.time() < 0:
+            self.next_obstacle_generate_time = time.time() + random.randint(1, 6)
+            self.array_obstacles.append(
+                Obstacle(self, 90, 35, random.randint(1,3))
+            )
         self.dino.update()
         self.stdscr.refresh()
 
